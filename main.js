f8a627a0b1c608958fc919e6075b6d325fa4e5e7
Demuno12A karollayne <karollaynebaptista@gmail.com>
2025-06-05 03:05:37 +0000

    início da função removeItem() para remover itens do array

--- main.js
+++ main.js
@@ -34,17 +34,28 @@ function showItemsList() {
                 <img src="./assets/checked.svg" alt="checked">
             </div>
             <label for="item-${index}">${item.name}</label>
+            kcmk 
             <label for="item-${index}"onclick="checkItem('${item.name}')>${item.name}</label>
         </div>
         <button>
+          <button onclick="removeItem('${item.name}')">
             <img src="./assets/trash-icon.svg" alt="trash icon">
-        </button>
-    `
+        </button> `
     })
 }
 
 function checkItem(itemName) {
     const item = item.find((item) => item.name === itemName)
     item.checked = !item.checked
+     showItemsList()
+}
+
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+
+    if(itemIndex !== -1) {
+        items.splice(itemIndex, 1)
+    }
+
     showItemsList()
 }  
